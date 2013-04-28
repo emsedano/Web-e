63146440094392aef6d0ed48412eddb35c2eee92
Francisco Ivan Eusebio Perez <ing.ivanep05@gmail.com>
2013-04-27 22:49:39 -0500

    2do commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WedE::Application.config.secret_token = 'f3520a54002e550541b386e18897c20af80fc408e8de8b51bcfc7219b93636ac5b66da475c21b7d528cce09ff89604fdd409dbfebb40d6a7965df8c53768d201'
