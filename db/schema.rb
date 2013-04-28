2bffdb17c143f060d3a304b31a655bd5806dce02
Francisco Ivan Eusebio Perez <ing.ivanep05@gmail.com>
2013-04-28 11:48:15 -0500

    Se corrigio el tipo de dato de la fecha de boda

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428163540) do
+ActiveRecord::Schema.define(:version => 20130428163201) do
 
   create_table "proveedors", :force => true do |t|
     t.string   "nombre"
@@ -22,17 +22,6 @@ ActiveRecord::Schema.define(:version => 20130428163540) do
     t.string   "nombre_responsable"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
-  end
-
-  create_table "usuarios", :force => true do |t|
-    t.string   "nombre"
-    t.string   "email"
-    t.string   "ciudad"
-    t.date     "fecha"
-    t.string   "telefono"
-    t.integer  "presupuesto"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
   end
 
 end
